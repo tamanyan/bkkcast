c4e849724eef1083a128b76c763aef6ea856ae8c
Taketo Yoshida <tamanyan.sss@gmail.com>
2018-07-03 11:22:38 +0700

    Update submodule

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,9 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project. 
 hugo # if using a theme, replace by `hugo -t <yourtheme>`
 
-#↑ここを変更しましょう！！
-
 # Go To Public folder
 cd public
+
 # Add changes to git.
 git add -A
 
@@ -17,6 +16,7 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
 git commit -m "$msg"
 
 #Push source and build repos.
